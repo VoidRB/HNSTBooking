e51d15a95056b64f1b9cf3445db63e9b59b9fe70
NinesVoid <ahmedalkhair47@gmail.com>
2025-01-11 19:17:03 +0300

    chore: Made the login/register page a bit better and more user friendly

--- client/src/components/RegisterForm.vue
+++ client/src/components/RegisterForm.vue
@@ -5,12 +5,18 @@ import { reactive } from 'vue'
 
 const user = reactive({
   email: '',
+  name: '',
+  gender: 'Gender',
+  age: '',
   password: '',
 })
 const register = async () => {
   try {
     const response = await axios.post('/api/register', {
       email: user.email,
+      name: user.name,
+      gender: user.gender,
+      age: user.age,
       password: user.password,
     })
     router.push({ name: 'AppointmentView' })
@@ -22,11 +28,34 @@ const register = async () => {
 <template>
   <form class="flex flex-col items-center gap-2">
     <label for="LoginForm">Register</label>
+
     <input
       v-model="user.email"
-      type="text"
+      type="email"
       placeholder="Email"
       autocomplete="email"
+      class="input input-bordered w-full max-w-xs"
+    />
+    <input
+      v-model="user.name"
+      type="text"
+      placeholder="Name"
+      autocomplete="name"
+      class="input input-bordered w-full max-w-xs"
+    />
+    <select v-model="user.gender" class="select select-bordered w-full max-w-xs">
+      <option disabled selected>Gender</option>
+      <option>Male</option>
+      <option>Female</option>
+      <option>Other</option>
+    </select>
+    <input
+      v-model="user.age"
+      min="0"
+      max="100"
+      type="number"
+      placeholder="Age"
+      autocomplete="age"
       class="input input-bordered w-full max-w-xs"
     />
     <input
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,12 +1,33 @@
 <script setup lang="ts">
 import LoginForm from '@/components/LoginForm.vue'
 import RegisterForm from '@/components/RegisterForm.vue'
+import { ref } from 'vue'
+
+const formState = ref(true)
+
+const changeForms = () => {
+  formState.value = !formState.value
+}
 </script>
 
 <template>
-  <div class="flex h-full w-full items-center justify-center gap-10">
-    <LoginForm />
-    <section>Or</section>
-    <RegisterForm />
+  <div class="flex h-full w-full flex-col items-center justify-center gap-10">
+    <section class="fixed top-24 flex flex-col">
+      <h1 class="text-center">Click here to change forms</h1>
+      <i class="pi pi-arrow-down self-center"></i>
+      <input
+        type="checkbox"
+        class="toggle mt-5 self-center"
+        :checked="formState"
+        @click="changeForms()"
+      />
+    </section>
+
+    <section v-if="formState">
+      <LoginForm />
+    </section>
+    <section v-else>
+      <RegisterForm />
+    </section>
   </div>
 </template>
