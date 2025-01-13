9755c4b98b8553d3ffc8ce33b50aacfdb5bb17b8
Nabil-249 <NabilAhmedIbrahim@outlook.com>
2025-01-13 16:34:07 +0300

    chore: extracted variables that will be used on both ends

---
+++ shared/roles.ts
@@ -0,0 +1,28 @@
+const guestRole = {
+  roleID: 0,
+  roleName: 'Guest'
+} as const
+
+const beneficiaryRole = {
+  roleID: 1,
+  roleName: 'Beneficiary'
+} as const
+
+const peerSupporterRole = {
+  roleID: 2,
+  roleName: 'Peer Supporter'
+} as const
+
+const coordinatorRole = {
+  roleID: 3,
+  roleName: 'Coordinator'
+} as const
+
+export const systemRoles = {
+  guest: guestRole,
+  beneficiary: beneficiaryRole,
+  peerSupporter: peerSupporterRole,
+  coordinator: coordinatorRole
+} as const
+
+export type SystemRoles = typeof systemRoles
