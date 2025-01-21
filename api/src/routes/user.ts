cbad33e215744be9ffe330b6961b5e8a3d66879b
NinesVoid <ahmedalkhair47@gmail.com>
2025-01-21 14:00:04 +0300

    ref: merged the user related routes

--- api/src/routes/user.ts
+++ api/src/routes/user.ts
@@ -2,19 +2,29 @@ import { Hono } from "hono";
 import { HonoContextBindings } from "..";
 import { authenticateMiddleware } from "../middlewares/authenticate";
 import { getUser } from "../db/getUser";
+import { getUsersbyRole } from "../db/getUsersByRole";
 
 const userRoute = new Hono<HonoContextBindings>();
 
 // Authenticating the token in the cookie
 userRoute.use(authenticateMiddleware);
 
-userRoute.get("/dashboard", async (ctx) => {
+userRoute.get("/single", async (ctx) => {
 	const currentUser = await getUser({
 		ctx: ctx,
 		id: ctx.get("CurrentUserID"),
 	});
 
 	return ctx.json(currentUser, 200);
+});
+
+userRoute.post("/all", async (ctx) => {
+	const allUsersByRole = await getUsersbyRole({
+		ctx: ctx,
+		roleId: ctx.get("UserRole"),
+	});
+
+	return ctx.json(allUsersByRole, 200);
 });
 
 export default userRoute;
